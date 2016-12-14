6c67ab7848ce966c2b2861598c46e98eb6aa5c93
Camilo Enrique Rivera <camilo.rive007@gmail.com>
2016-12-14 09:34:07 -0500

    Add files via upload

---
+++ LearningPython/vowelCounter.py
@@ -0,0 +1,7 @@
+s = 'azcbobobegghakl'
+vowelCount = 0
+for letter in s:
+    if letter == 'a' or letter == 'e'or letter == 'i' or letter == 'o' or letter == 'u':
+        vowelCount +=1
+        
+print ('Number of vowels: '+ str(vowelCount))
